46962bbb9231166f4e36c9cf476161e18b0fa612
tannjieee <86846878+tannjieee@users.noreply.github.com>
2026-06-03 23:04:46 +0800

    Add wheelfoot domain randomization privileged obs config

--- legged_gym/envs/wheelfoot_flat/wheelfoot_flat_config.py
+++ legged_gym/envs/wheelfoot_flat/wheelfoot_flat_config.py
@@ -1,42 +1,16 @@
-# SPDX-FileCopyrightText: Copyright (c) 2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice, this
-# list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# Copyright (c) 2021 ETH Zurich, Nikita Rudin
 from legged_gym.envs.base.base_config import BaseConfig
 
 
 class BipedCfgWF(BaseConfig):
     class env:
         num_envs = 8192
-        num_observations = 30 + 6 - 2 - 4 - 2  # +6 means wheel obs,-2 means sin&cos clock, -4 means gait para nums -2 means wheels pos
-        num_critic_observations = 3 + num_observations
-        num_height_samples = 117
         num_actions = 8
+        num_observations = 30 + 6 - 2 - 4 - 2  # +6 means wheel obs,-2 means sin&cos clock, -4 means gait para nums -2 means wheels pos
+        # friction, restitution, base mass, base COM, inertia, action delay, IMU offset,
+        # Kp scale, Kd scale, motor torque scale, default DOF offset, and push force.
+        num_privileged_domain_rand_obs = 1 + 1 + 1 + 3 + 1 + 1 + 4 + num_actions * 4 + 3
+        num_critic_observations = 3 + num_observations + num_privileged_domain_rand_obs
+        num_height_samples = 117
         env_spacing = 3.0  # not used with heightfields/trimeshes
         send_timeouts = True  # send time out information to the algorithm
         episode_length_s = 20  # episode length in seconds
@@ -175,14 +149,12 @@ class BipedCfgWF(BaseConfig):
         penalize_contacts_on = ["knee", "hip"]
         terminate_after_contacts_on = ["abad", "base"]
         disable_gravity = False
-        collapse_fixed_joints = True  # merge bodies connected by fixed joints. Specific fixed joints can be kept by adding " <... dont_collapse="true">
+        collapse_fixed_joints = True
         fix_base_link = False  # fixe the base of the robot
         default_dof_drive_mode = 3  # see GymDofDriveModeFlags (0 is none, 1 is pos tgt, 2 is vel tgt, 3 effort)
         self_collisions = 0  # 1 to disable, 0 to enable...bitwise filter
         replace_cylinder_with_capsule = True  # replace collision cylinders with capsules, leads to faster/more stable simulation
-        flip_visual_attachments = (
-            False  # Some .obj meshes must be flipped from y-up to z-up
-        )
+        flip_visual_attachments = False
 
         density = 0.001
         angular_damping = 0.0
@@ -324,7 +296,7 @@ class BipedCfgWF(BaseConfig):
             num_position_iterations = 4
             num_velocity_iterations = 0
             contact_offset = 0.01  # [m]
-            rest_offset = 0.0  # [m]
+            rest_offset = 0.0
             bounce_threshold_velocity = 0.5  # 0.5 [m/s]
             max_depenetration_velocity = 1.0
             max_gpu_contact_pairs = 2**23  # 2**24 -> needed for 8000 envs and more
