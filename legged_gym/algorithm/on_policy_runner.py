b1419358550365310d759874ed8ad70736028f81
tannjieee <86846878+tannjieee@users.noreply.github.com>
2026-06-06 15:21:31 +0800

    Wire NP3O runner support

--- legged_gym/algorithm/on_policy_runner.py
+++ legged_gym/algorithm/on_policy_runner.py
@@ -1,31 +1,6 @@
 # SPDX-FileCopyrightText: Copyright (c) 2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice, this
-# list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
 # Copyright (c) 2021 ETH Zurich, Nikita Rudin
 
 import time
@@ -39,6 +14,7 @@ import torch
 import numpy as np
 
 from .ppo import PPO
+from .np3o import NP3O
 from .mlp_encoder import MLP_Encoder
 from .actor_critic import ActorCritic
 from legged_gym.envs.vec_env import VecEnv
@@ -61,11 +37,9 @@ class OnPolicyRunner:
         if self.alg_cfg["critic_take_latent"]:
             num_critic_obs += encoder.num_output_dim
 
-        actor_critic_class = eval(self.cfg["policy_class_name"])  # ActorCritic
+        actor_critic_class = eval(self.cfg["policy_class_name"])
         actor_critic: ActorCritic = actor_critic_class(
-            self.env.num_obs
-            + encoder.num_output_dim
-            + self.env.num_commands,
+            self.env.num_obs + encoder.num_output_dim + self.env.num_commands,
             num_critic_obs,
             self.env.num_actions,
             **self.policy_cfg,
@@ -83,7 +57,6 @@ class OnPolicyRunner:
         self.num_steps_per_env = self.cfg["num_steps_per_env"]
         self.save_interval = self.cfg["save_interval"]
 
-        # init storage and model
         self.alg.init_storage(
             self.env.num_envs,
             self.num_steps_per_env,
@@ -101,20 +74,16 @@ class OnPolicyRunner:
             1, dtype=torch.float, device=self.device, requires_grad=False
         )
 
-        # Log
         self.log_dir = log_dir
         self.writer = None
         self.tot_timesteps = 0
         self.tot_time = 0
         self.current_learning_iteration = 0
 
-        # _, _ = self.env.reset()
         _ = self.env.reset()
 
     def learn(self, num_learning_iterations, init_at_random_ep_len=False):
-        # initialize writer
         if self.log_dir is not None and self.writer is None:
-            # Launch either Tensorboard or Wandb & Tensorboard summary writer(s), default: Tensorboard.
             self.logger_type = self.cfg.get("logger", "tensorboard")
             self.logger_type = self.logger_type.lower()
 
@@ -137,15 +106,13 @@ class OnPolicyRunner:
                 self.env.episode_length_buf, high=int(self.env.max_episode_length)
             )
         obs, obs_history, commands, critic_obs = self.env.get_observations()
-        # critic_obs = obs
         obs, obs_history, commands, critic_obs = (
             obs.to(self.device),
             obs_history.to(self.device),
             commands.to(self.device),
             critic_obs.to(self.device),
         )
-        # ???
-        self.alg.actor_critic.train()  # switch to train mode (for dropout for example)
+        self.alg.actor_critic.train()
 
         ep_infos = []
         rewbuffer = deque(maxlen=100)
@@ -160,25 +127,30 @@ class OnPolicyRunner:
         tot_iter = self.current_learning_iteration + num_learning_iterations
         for it in range(self.current_learning_iteration, tot_iter):
             start = time.time()
-            # Rollout
             with torch.inference_mode():
                 for i in range(self.num_steps_per_env):
                     actions = self.alg.act(obs, obs_history, commands, critic_obs)
-                    # add critic_obs_buf to step returns, make sure it updates in every for loop
-                    (obs, rewards, dones, infos, obs_history, commands, critic_obs_buf) = self.env.step(actions)
-                    # critic_obs = obs
+                    (
+                        obs,
+                        rewards,
+                        dones,
+                        infos,
+                        obs_history,
+                        commands,
+                        critic_obs_buf,
+                    ) = self.env.step(actions)
+
                     obs, obs_history, commands, critic_obs, rewards, dones = (
                         obs.to(self.device),
                         obs_history.to(self.device),
                         commands.to(self.device),
-                        critic_obs_buf.to(self.device), # critic_obs.to(self.device),
+                        critic_obs_buf.to(self.device),
                         rewards.to(self.device),
                         dones.to(self.device),
                     )
-                    self.alg.process_env_step(rewards, dones, infos, obs)
+                    self.alg.process_env_step(rewards, dones, infos, obs, env=self.env)
 
                     if self.log_dir is not None:
-                        # Book keeping
                         if "episode" in infos:
                             ep_infos.append(infos["episode"])
                         cur_reward_sum += rewards
@@ -195,8 +167,6 @@ class OnPolicyRunner:
 
                 stop = time.time()
                 collection_time = stop - start
-
-                # Learning step
                 start = stop
 
                 critic_obs_ = torch.cat((critic_obs, commands), dim=-1)
@@ -208,12 +178,27 @@ class OnPolicyRunner:
                 else:
                     self.alg.compute_returns(critic_obs_)
 
+            update_results = self.alg.update()
             (
                 mean_value_loss,
                 mean_extra_loss,
                 mean_surrogate_loss,
                 mean_kl,
-            ) = self.alg.update()
+            ) = update_results[:4]
+            if len(update_results) >= 9:
+                (
+                    mean_cost_value_loss,
+                    mean_cost_surrogate_loss,
+                    mean_penalty_loss,
+                    mean_cost,
+                    penalty_coef,
+                ) = update_results[4:9]
+            else:
+                mean_cost_value_loss = 0.0
+                mean_cost_surrogate_loss = 0.0
+                mean_penalty_loss = 0.0
+                mean_cost = 0.0
+                penalty_coef = 0.0
             stop = time.time()
             learn_time = stop - start
 
@@ -240,7 +225,6 @@ class OnPolicyRunner:
             for key in locs["ep_infos"][0]:
                 infotensor = torch.tensor([], device=self.device)
                 for ep_info in locs["ep_infos"]:
-                    # handle scalar and zero dimensional tensor infos
                     if not isinstance(ep_info[key], torch.Tensor):
                         ep_info[key] = torch.Tensor([ep_info[key]])
                     if len(ep_info[key].shape) == 0:
@@ -249,7 +233,7 @@ class OnPolicyRunner:
                 value = torch.mean(infotensor)
                 self.writer.add_scalar("Episode/" + key, value, locs["it"])
                 ep_string += f"""{f'{key}:':>{pad}} {value:.4f}\n"""
-        # mean_std = self.alg.actor_critic.std.mean()
+
         mean_std = torch.exp(self.alg.actor_critic.logstd).mean()
         fps = int(
             self.num_steps_per_env
@@ -267,6 +251,17 @@ class OnPolicyRunner:
         self.writer.add_scalar("Loss/learning_rate", self.alg.learning_rate, locs["it"])
         self.writer.add_scalar("Policy/mean_noise_std", mean_std.item(), locs["it"])
         self.writer.add_scalar("Policy/mean_kl", locs["mean_kl"], locs["it"])
+        if "mean_cost_value_loss" in locs:
+            self.writer.add_scalar(
+                "Loss/cost_value_function", locs["mean_cost_value_loss"], locs["it"]
+            )
+            self.writer.add_scalar(
+                "Loss/cost_surrogate", locs["mean_cost_surrogate_loss"], locs["it"]
+            )
+            self.writer.add_scalar("Loss/penalty", locs["mean_penalty_loss"], locs["it"])
+            self.writer.add_scalar("Cost/mean_cost", locs["mean_cost"], locs["it"])
+            self.writer.add_scalar("Cost/penalty_coef", locs["penalty_coef"], locs["it"])
+
         self.writer.add_scalar("Perf/total_fps", fps, locs["it"])
         self.writer.add_scalar(
             "Perf/collection time", locs["collection_time"], locs["it"]
@@ -281,9 +276,7 @@ class OnPolicyRunner:
                 statistics.mean(locs["lenbuffer"]),
                 locs["it"],
             )
-            if (
-                self.logger_type != "wandb"
-            ):  # wandb does not support non-integer x-axis logging
+            if self.logger_type != "wandb":
                 self.writer.add_scalar(
                     "Train/mean_reward/time",
                     statistics.mean(locs["rewbuffer"]),
@@ -295,14 +288,13 @@ class OnPolicyRunner:
                     self.tot_time,
                 )
 
-        str = f" \033[1m Learning iteration {locs['it']}/{self.current_learning_iteration + locs['num_learning_iterations']} \033[0m "
+        title = f" \033[1m Learning iteration {locs['it']}/{self.current_learning_iteration + locs['num_learning_iterations']} \033[0m "
 
         if len(locs["rewbuffer"]) > 0:
             log_string = (
                 f"""{'#' * width}\n"""
-                f"""{str.center(width, ' ')}\n\n"""
-                f"""{'Computation:':>{pad}} {fps:.0f} steps/s (collection: {locs[
-                            'collection_time']:.3f}s, learning {locs['learn_time']:.3f}s)\n"""
+                f"""{title.center(width, ' ')}\n\n"""
+                f"""{'Computation:':>{pad}} {fps:.0f} steps/s (collection: {locs['collection_time']:.3f}s, learning {locs['learn_time']:.3f}s)\n"""
                 f"""{'Value function loss:':>{pad}} {locs['mean_value_loss']:.4f}\n"""
                 f"""{'Surrogate loss:':>{pad}} {locs['mean_surrogate_loss']:.4f}\n"""
                 f"""{'Mean action noise std:':>{pad}} {mean_std.item():.4f}\n"""
@@ -310,20 +302,24 @@ class OnPolicyRunner:
                 f"""{'Mean reward:':>{pad}} {statistics.mean(locs['rewbuffer']):.2f}\n"""
                 f"""{'Mean episode length:':>{pad}} {statistics.mean(locs['lenbuffer']):.2f}\n"""
             )
-            #   f"""{'Mean reward/step:':>{pad}} {locs['mean_reward']:.2f}\n"""
-            #   f"""{'Mean episode length/episode:':>{pad}} {locs['mean_trajectory_length']:.2f}\n""")
         else:
             log_string = (
                 f"""{'#' * width}\n"""
-                f"""{str.center(width, ' ')}\n\n"""
-                f"""{'Computation:':>{pad}} {fps:.0f} steps/s (collection: {locs[
-                            'collection_time']:.3f}s, learning {locs['learn_time']:.3f}s)\n"""
+                f"""{title.center(width, ' ')}\n\n"""
+                f"""{'Computation:':>{pad}} {fps:.0f} steps/s (collection: {locs['collection_time']:.3f}s, learning {locs['learn_time']:.3f}s)\n"""
                 f"""{'Value function loss:':>{pad}} {locs['mean_value_loss']:.4f}\n"""
                 f"""{'Surrogate loss:':>{pad}} {locs['mean_surrogate_loss']:.4f}\n"""
                 f"""{'Mean action noise std:':>{pad}} {mean_std.item():.2f}\n"""
             )
-            #   f"""{'Mean reward/step:':>{pad}} {locs['mean_reward']:.2f}\n"""
-            #   f"""{'Mean episode length/episode:':>{pad}} {locs['mean_trajectory_length']:.2f}\n""")
+
+        if "mean_cost" in locs:
+            log_string += (
+                f"""{'Mean cost:':>{pad}} {locs['mean_cost']:.4f}\n"""
+                f"""{'Cost value loss:':>{pad}} {locs['mean_cost_value_loss']:.4f}\n"""
+                f"""{'Cost surrogate loss:':>{pad}} {locs['mean_cost_surrogate_loss']:.4f}\n"""
+                f"""{'Penalty loss:':>{pad}} {locs['mean_penalty_loss']:.4f}\n"""
+                f"""{'Penalty coef:':>{pad}} {locs['penalty_coef']:.4f}\n"""
+            )
 
         log_string += ep_string
         log_string += (
@@ -331,8 +327,7 @@ class OnPolicyRunner:
             f"""{'Total timesteps:':>{pad}} {self.tot_timesteps}\n"""
             f"""{'Iteration time:':>{pad}} {iteration_time:.2f}s\n"""
             f"""{'Total time:':>{pad}} {self.tot_time:.2f}s\n"""
-            f"""{'ETA:':>{pad}} {self.tot_time / (locs['it'] + 1) * (
-                               locs['num_learning_iterations'] - locs['it']):.1f}s\n"""
+            f"""{'ETA:':>{pad}} {self.tot_time / (locs['it'] + 1) * (locs['num_learning_iterations'] - locs['it']):.1f}s\n"""
         )
         print(log_string)
 
@@ -350,7 +345,9 @@ class OnPolicyRunner:
 
     def load(self, path, load_optimizer=False):
         loaded_dict = torch.load(path)
-        self.alg.actor_critic.load_state_dict(loaded_dict["model_state_dict"])
+        self.alg.actor_critic.load_state_dict(
+            loaded_dict["model_state_dict"], strict=False
+        )
         self.alg.encoder.load_state_dict(loaded_dict["encoder_state_dict"])
         if load_optimizer:
             self.alg.optimizer.load_state_dict(loaded_dict["optimizer_state_dict"])
@@ -358,19 +355,19 @@ class OnPolicyRunner:
         return loaded_dict["infos"]
 
     def get_inference_policy(self, device=None):
-        self.alg.actor_critic.eval()  # switch to evaluation mode (dropout for example)
+        self.alg.actor_critic.eval()
         if device is not None:
             self.alg.actor_critic.to(device)
         return self.alg.actor_critic.act_inference
 
     def get_inference_encoder(self, device=None):
-        self.alg.encoder.eval()  # switch to evaluation mode (dropout for example)
+        self.alg.encoder.eval()
         if device is not None:
             self.alg.encoder.to(device)
         return self.alg.encoder.encode
 
     def get_actor_critic(self, device=None):
-        self.alg.actor_critic.eval()  # switch to evaluation mode (dropout for example)
+        self.alg.actor_critic.eval()
         if device is not None:
             self.alg.actor_critic.to(device)
         return self.alg.actor_critic
